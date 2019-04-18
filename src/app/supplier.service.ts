988c2ce02336fbbda387a6d92dbc89f144df589b
Arshad Aboo <arshad.aboo@entelect.co.za>
2019-04-18 13:15:16 +0200

    cleanup code

--- src/app/supplier.service.ts
+++ src/app/supplier.service.ts
@@ -9,18 +9,12 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SupplierService {
 
-  // done without 'Observable' and 'of'
-  // getSuppliers(): Supplier[] {
-  //   return SUPPLIERS;
-  // }
-
   suppliersUrl = 'http://localhost:3030/suppliers';
 
   getSuppliersFromUrl() {
     return this.http.get(this.suppliersUrl);
   }
 
-  // done with 'Observable' and 'of'
   getSuppliers(): Observable<Supplier[]> {
     return of(SUPPLIERS);
   }
--- src/app/suppliers/suppliers.component.ts
+++ src/app/suppliers/suppliers.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Supplier } from '../supplier';
-//import { SUPPLIERS } from '../mock-suppliers'; <-- Components should not be responsible for getting this, should be provided by a service
 import { SupplierService } from '../supplier.service';
 import { Observable } from 'rxjs';
 
@@ -11,44 +10,20 @@ import { Observable } from 'rxjs';
 })
 export class SuppliersComponent implements OnInit {
 
-  // suppliers = SUPPLIERS; <-- Components should not be responsible for getting this, should be provided by a service
   suppliers: Supplier[];
 
   selectedSupplier: Supplier;
 
   response: any;
 
-  // Without 'Observable' and 'of'
-  // getSuppliers(): void {
-  //   this.suppliers = this.supplierService.getSuppliers();
-  // }
-
-  getSuppliers(): void {
-    // console.log('getSuppliers async method');
-    this.supplierService.getSuppliers()
-      .subscribe(suppliers => this.suppliers = suppliers);
-  }
-
-  // getSuppliersFromUrl(): void {
-  //   this.supplierService.getSuppliersFromUrl()
-  //     .subscribe((res) => { 
-  //       console.log("res: " + res);
-  //       this.response = res;
-  //     });
-  // }
-
   getSuppliersFromUrl(): void {
     this.supplierService.getSuppliersFromUrl()
       .subscribe({
         next: (suppliers: any) => {
           this.suppliers = suppliers.data;
-          // console.log(`this.suppliers ${this.suppliers}`);
-          // debugger;
-          // console.log("data: " + JSON.stringify(suppliers.data));
         }
       });
   }
-
 
   onSelect(supplier: Supplier): void {
     this.selectedSupplier = supplier;
@@ -57,7 +32,6 @@ export class SuppliersComponent implements OnInit {
   constructor(private supplierService: SupplierService) { }
 
   ngOnInit() {
-    // this.getSuppliers();
     this.getSuppliersFromUrl();
   }
 
