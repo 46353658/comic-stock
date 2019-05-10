eba33512832217591728b069173e2020a2840343
Arshad Aboo <arshad.aboo@entelect.co.za>
2019-05-10 14:39:28 +0200

    pagination and search implemented for issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { SuppliersComponent } from './suppliers/suppliers.component';
-// import { SupplierDetailComponent } from './supplier/supplier-detail/supplier-detail.component';
 import { HttpClientModule } from '@angular/common/http';
-// import { IssuesComponent } from './issue/issues/issues.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import { NgxPaginationModule } from 'ngx-pagination';
 import { MatButtonModule } from '@angular/material/button';
-// import { ConfirmationPopoverModule } from 'angular-confirmation-popover';
 import { SupplierModule } from './supplier/supplier.module';
 import { IssueModule } from './issue/issue.module';
 
 @NgModule({
   declarations: [
     AppComponent
-    // SuppliersComponent,
-    // SupplierDetailComponent,
-    // IssuesComponent
   ],
   imports: [
     BrowserModule,
@@ -28,12 +19,7 @@ import { IssueModule } from './issue/issue.module';
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    // Ng2SearchPipeModule,
-    // NgxPaginationModule,
     MatButtonModule,
-    // ConfirmationPopoverModule.forRoot({
-    //   confirmButtonType: 'danger'
-    // }),
     SupplierModule,
     IssueModule
   ],
--- src/app/issue/issue.module.ts
+++ src/app/issue/issue.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IssuesComponent } from '../issue/issues/issues.component';
+import { FormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
     IssuesComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    Ng2SearchPipeModule,
+    NgxPaginationModule
   ]
 })
 export class IssueModule { }
